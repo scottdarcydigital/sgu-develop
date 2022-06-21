3c96e1af6396a97be1b48ab5cffbbb803248e464
canstudios-scottd <scottdarcydigital@gmail.com>
2022-06-21 18:53:08 +0100

    Radio Pick up Script complete
    However, the texturing on the Radio still needs to be completed

---
+++ Assets/Scripts/RadioPickUp.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RadioPickUp : MonoBehaviour
+{
+    // Turn on the collider logic for the first console only after the radio has been picked up
+    public GameObject FirstConsole;
+    public GameObject Radio;
+    public bool nearRadio;
+
+    void Start()
+    {
+        Radio = this.gameObject;
+        FirstConsole.GetComponent<CapsuleCollider>().enabled = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            nearRadio = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            nearRadio = false;
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            if (nearRadio)
+            {
+                FirstConsole.GetComponent<CapsuleCollider>().enabled = true;
+                Destroy(this.gameObject);
+            }
+        }
+    }
+}
