05653dfb340dd61d11cef72a6f2e4bee481fce0b
canstudios-scottd <scottdarcydigital@gmail.com>
2022-07-28 15:35:08 +0100

    Cutscene Level added
    Cutscene Level and animations added to a video plane in that level. Player must be destroyed on the load of this level.
    
    At this point it simply plays a video on a plane element

--- Assets/Scripts/DestroyOnLoadWithDelay.cs
+++ Assets/Scripts/DestroyOnLoadWithDelay.cs
@@ -5,35 +5,42 @@ using UnityEngine;
 public class DestroyOnLoadWithDelay : MonoBehaviour
 {
 
-    //public GameObject Player;
-    //public GameObject PlayerCamera;
-    //public GameObject FadeOutCanvas;
+    public GameObject Player;
+    public GameObject PlayerCamera;
+    public GameObject FadeOutCanvas;
+    public GameObject FadeInCanvas;
     public GameObject VideoPlane;
 
     // Start is called before the first frame update
     void Awake()
     {
-        //FadeOutCanvas = GameObject.FindWithTag("LevelFadeOut");
-        //Player = GameObject.FindWithTag("Player");
-        //PlayerCamera = GameObject.FindWithTag("MainCamera");
-        //PlayerCamera.SetActive(false);
-        //Invoke("DestroyPlayer", 2);
+        FadeOutCanvas = GameObject.FindWithTag("LevelFadeOut");
+        Player = GameObject.FindWithTag("Player");
+        PlayerCamera = GameObject.FindWithTag("MainCamera");
+        PlayerCamera.SetActive(false);
+        Invoke("DestroyPlayer", 1.0f);
     }
 
     void Start()
     {
-        //Invoke("DestroyFadeOut", 0.5f);
-        //Invoke("VideoPlaneActive", 0.01f);
+        Invoke("DestroyFadeOut", 1.0f);
+        //Invoke("DestroyFadeIn", 2.5f);
+        //Invoke("VideoPlaneActive", 1.5f);
     }
 
-    //void DestroyPlayer()
-    //{
-    //    Destroy(Player);
-    //}
+    void DestroyPlayer()
+    {
+        Destroy(Player);
+    }
 
-    //void DestroyFadeOut()
+    void DestroyFadeOut()
+    {
+        Destroy(FadeOutCanvas);
+    }
+
+    //void DestroyFadeIn()
     //{
-    //    Destroy(FadeOutCanvas);
+    //    Destroy(FadeInCanvas);
     //}
 
     //void VideoPlaneActive()
